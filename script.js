d82feec47503ea5d29a6624d13e2fb3d14848e17
jesscorrao-droid <jess.corrao@gmail.com>
2026-07-09 14:50:14 +0200

    Implement video modal for trailer playback
    Add modal functionality for video trailer display.

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-document.getElementById("watchTrailer").addEventListener("click",()=>{
+const modal=document.getElementById("videoModal");
 
-alert("Qui apriremo il trailer del matrimonio.");
+const btn=document.getElementById("watchTrailer");
 
-});
+const close=document.querySelector(".close");
+
+btn.onclick=function(){
+
+modal.style.display="block";
+
+}
+
+close.onclick=function(){
+
+modal.style.display="none";
+
+document.getElementById("trailerVideo").src=
+document.getElementById("trailerVideo").src;
+
+}
+
+window.onclick=function(event){
+
+if(event.target==modal){
+
+modal.style.display="none";
+
+document.getElementById("trailerVideo").src=
+document.getElementById("trailerVideo").src;
+
+}
+
+}
